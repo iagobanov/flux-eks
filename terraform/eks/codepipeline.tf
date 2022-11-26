5d4e2ebd926a3fb3962301d50d5f88883ad38d8e
Iago Banov <iagobanov@outlook.com>
2022-11-26 17:00:52 -0300

    Add build

--- terraform/eks/codepipeline.tf
+++ terraform/eks/codepipeline.tf
@@ -103,7 +103,7 @@ resource "aws_codebuild_project" "example" {
 
   environment {
     compute_type                = "BUILD_GENERAL1_MEDIUM"
-    image                       = "aws/codebuild/standard:1.0"
+    image                       = "aws/codebuild/standard:4.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
     environment_variable {
